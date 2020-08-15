b886f9db61b450f4ed0f1448aaa13e7d1c717ff7
Sylean <anson.prima@gmail.com>
2020-08-15 19:11:55 -0400

    Modified Navbar (Text Logo)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.scss';
 
 function Header() {
     return  (
-        <nav className="nav-header">
-          <ul className="nav-header-pages">
-            <li>
-              <NavLink exact to="/" aria-current="page">Home</NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/about" aria-current="page">About</NavLink>
-            </li>
-            <li>
-              <NavLink exact to="/users" aria-current="page">Users</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <header className="nav-header">
+          <Link to="/">
+            <h2 className="nav-header-logo">
+                Ambetek                
+            </h2>
+          </Link>
+          <nav className="nav-header-nav">
+            <ul className="nav-header-pages">
+              <li>
+                <NavLink exact to="/" aria-current="page">Home</NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/about" aria-current="page">About</NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/users" aria-current="page">Users</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
     );
 }
 
